5780016c1ad00dcae6124c1d31d8a51e686225c1
SKrpio <134825964+SKrpio@users.noreply.github.com>
2023-09-19 20:01:48 +1000

    switch use for get

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import "../pages/CSS/HomePage.css";
 import useProjects from "../hooks/useProjects";
 import ProjectCard from "../components/ProjectCard";
-
+import getProjects from "../api/get-projects";
 
 function HomePage() {
 
-    const { projects, isLoading, error } = useProjects();
+    const { projects, isLoading, error } = getProjects();
     console.log("projects", projects)
 
     return (
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,4 +5,6 @@ function LoginPage() {
     return <LoginForm />
 }
 
+
+
 export default LoginPage
