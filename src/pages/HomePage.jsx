09723d3595094a69fa1d608209e3ecef4b16ba5e
SKrpio <134825964+SKrpio@users.noreply.github.com>
2023-09-28 16:02:07 +1000

    logo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,27 @@
+import React from "react";
 import "../pages/CSS/HomePage.css";
 import useProjects from "../hooks/useProjects";
 import ProjectCard from "../components/ProjectCard";
-
+import logo from '../images/logo.png';
 
 function HomePage() {
 
     const { projects } = useProjects();
     console.log("projects", projects)
-
     return (
-        <div id="project-list">
-            {
-                projects.map((project, index) => {
-                    return <ProjectCard project={project} index={index} />
-                })
-            }
-        </div>
-    )
+        <>
+            <div className="top-image"> 
+                <img src={logo} alt="Background" />
+            </div>
+            <div id="project-list">
+                {projects.map((project, index) => {
+                    return <ProjectCard project={project} index={index} />;
+                })}
+            </div>
+        </>
+    );
 }
-export default HomePage
+
+export default HomePage;
+
 
