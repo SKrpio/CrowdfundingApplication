404ebd91f19699cadd07bcc5586220a202011391
SKrpio <134825964+SKrpio@users.noreply.github.com>
2023-10-05 17:16:25 +1000

    pledge wings

--- src/pages/Pledge.jsx
+++ src/pages/Pledge.jsx
@@ -1,13 +1,18 @@
 import "../pages/CSS/PledgePage.css";
 import CreatePledge from "../components/CreatePledge";
-
+import leftWing from '../images/leftWing.png';
+import rightWing from '../images/rightWing.png';
     
     
 function Pledge() {
     return (
+        <div>             
+            <img src={leftWing} className="wing left-wing" />
+            <img src={rightWing} className="wing right-wing"/>
         <div className="pledge-container">
         <CreatePledge />
         </div>
+        </div>        
     ) 
 }
 
